24ab461c2ab1cfd4b0600a60d79a1ff59244c1bf
Rixef <74892049+Rixef@users.noreply.github.com>
2022-01-10 20:23:34 -0800

    Update Functions.h
    Remove unused functions

--- OSKHighlights/OSKHighlights/Functions.h
+++ OSKHighlights/OSKHighlights/Functions.h
@@ -29,67 +29,6 @@ DWORD GetProcId(const wchar_t* procName)
 	return procId;
 }
 
-//example usage:
-//uintptr_t modBaseAddy = GetModuleBaseAddress(procId, L"NMS.exe");
-uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
-{
-	uintptr_t modBaseAddress = 0;
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
-	if (hSnap != INVALID_HANDLE_VALUE)
-	{
-		MODULEENTRY32 modEntry;
-		modEntry.dwSize = sizeof(modEntry);
-		if (Module32First(hSnap, &modEntry))
-		{
-			do
-			{
-				if (!_wcsicmp(modEntry.szModule, modName))
-				{
-					modBaseAddress = (uintptr_t)modEntry.modBaseAddr;
-					break;
-				}
-			} while (Module32Next(hSnap, &modEntry));
-		}
-	}
-	CloseHandle(hSnap);
-	return modBaseAddress;
-}
-
-//example usage:
-//std::vector<unsigned int> healthOffsets = {0x0, 0x08, 0x38C, 0x28, 0x374};
-//uintptr_t healthAddy = FindDMAAddy(hProc,LocalPlayerBasePtr,healthOffsets);
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
-{
-	uintptr_t addr = ptr;
-	for (int i = 0; i < offsets.size(); i++)
-	{
-		ReadProcessMemory(hProc, (LPCVOID)addr, &addr, sizeof(addr), 0);
-		addr += offsets[i];
-	}
-	return addr;
-}
-
-//example usage:
-//std::vector<unsigned int> healthAOB = {0x0F, 0x0, 0x0, 0x0, 0x13, 0x0, 0x0, 0x0, 0x03, 0x0, 0x0, 0x0, 0x05, 0x0, 0x0, 0x0, 0x14, 0x0, 0x0, 0x0, 0x1D, 0x0, 0x0, 0x0, 0x05, 0x0, 0x0, 0x0, 0x08, 0x0, 0x0, 0x0};
-//uintptr_t healthAddy = AOBScanModule();
-uintptr_t AOBScanModule()
-{
-	uintptr_t addr = 0;
-
-	return addr;
-}
-
-//example usage:
-//std::vector<unsigned int> healthAOB = {0x0F, 0x0, 0x0, 0x0, 0x13, 0x0, 0x0, 0x0, 0x03, 0x0, 0x0, 0x0, 0x05, 0x0, 0x0, 0x0, 0x14, 0x0, 0x0, 0x0, 0x1D, 0x0, 0x0, 0x0, 0x05, 0x0, 0x0, 0x0, 0x08, 0x0, 0x0, 0x0};
-//uintptr_t healthAddy = AOBScanRegion(0x10000000000,0x40000000000,healthAOB);
-//will scan from 0x10000000000 to 0x40000000000 for the array of bytes/unsigned int vector and return the address that matches it
-uintptr_t AOBScanRegion(uintptr_t startAddy, uintptr_t finishAddy, std::vector<unsigned int> aob)
-{
-	uintptr_t addr = 0;
-
-	return addr;
-}
-
 //usage:
 //sikey("hardware",SC_Q);//will press Q via a hardware scan code(SC)
 //sikey("virtual",VK_Q);//will press Q via a virtual key code(VK)
